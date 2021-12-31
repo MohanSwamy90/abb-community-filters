f51d4615525920d1324ebd76f94b3e9a4bad9c6a
MohanSwamy90 <96230468+MohanSwamy90@users.noreply.github.com>
2021-12-31 15:13:04 +0530

    Update init-create-topic-button.js

--- javascripts/discourse/api-initializers/init-create-topic-button.js
+++ javascripts/discourse/api-initializers/init-create-topic-button.js
@@ -1,1 +1,13 @@
 
+import { apiInitializer } from "discourse/lib/api";
+import { h } from 'virtual-dom';
+
+export default apiInitializer("0.8", (api) => {    
+  api.createWidget("create-topic-widget", {
+    tagName: "div.create-topic-widget",
+
+    html() {           
+      return  h("button.start-discussion","start-discussion");;
+    },
+  });
+});
